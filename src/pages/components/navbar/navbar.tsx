c8fe8da8b79da6eb78f239c7193c7480307f7602
jjennbui <jbui22@my.bcit.ca>
2024-02-05 21:19:16 -0800

    updated navbar

--- src/pages/components/navbar/navbar.tsx
+++ src/pages/components/navbar/navbar.tsx
@@ -1,22 +1,18 @@
 
 import Image from 'next/image';
-import styles from '../../components/navbar/navbar.module.css';
 import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <div className={styles.nav}>
+    <div className="flex justify-between items-center p-12 text-white fixed top-0 left-0 right-0 w-full">
       <Link href="/">
         <Image src="/assets/logo/stardustLogo.svg" width={100} height={100} alt="Logo" />
       </Link>
-
       <div >
         <a href="/form" >
           Your Horoscopes
         </a>
       </div>
-
-
     </div>
   );
 }
