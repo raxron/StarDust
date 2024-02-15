9fe782b73755a9c79389ff1b1ef606cb2aedcc95
RachelX <113003027+raxron@users.noreply.github.com>
2024-02-15 11:07:03 -0800

    api 2 added; data updated;

--- src/pages/components/Carousel.tsx
+++ src/pages/components/Carousel.tsx
@@ -2,61 +2,65 @@ import Image from "next/image";
 import { useState } from "react";
 import Data from "../../../data/carouselData.json"
 import { ZodiacSign } from "../../../typing";
+import ZodiacInfo from "./ZodiacInfo";
 
 export default function Carousel() {
-   const [zodiac, setZodiac] = useState<ZodiacSign[]>(Data.signs);
-   const [clickedSign, setClickedSign] = useState("unclicked");
-   const [clickedSummary, setClickedSummary] = useState("none");
-   const links = '/assets/signs/';
-   
-   return (
-       <div 
-       className="min-h-screen flex flex-col items-center justify-center">
-           <div 
-           className="text-dark relative z-10">
-            {clickedSign === "unclicked" ?
-               <h1 className="text-3xl mb-4 text-white">
-                   Click to see each zodiac sign
-               </h1> :
-               <div className="flex items-center jusity-center flex-col">
-                   <p>{clickedSign}</p>
-                   <p>{clickedSummary}</p>
-               </div>
-           }
-           </div>
-       
-           <div className="absolute flex items-center justify-center"
-           style={{
-               height:'37em',
-               width:'37em',
-               background: `${clickedSign === "unclicked" ? "radial-gradient(50% 50% at 50% 50%, #3B3170 8.28%, rgba(23, 18, 51, 0.54) 75%)":"radial-gradient(50% 50% at 50% 50%, #CDCBC0 0%, #CDCBC0 55%, rgba(205, 203, 192, 0.06) 100%)"}`,
-               borderRadius:'100%'
-           }}>
-               {zodiac.map((sign, index) => (
-                   <div
-                       onClick={() => { setClickedSign(sign.name), setClickedSummary(sign.summary) }}
-                       key={sign.id}
-                       className="absolute w-16 h-16 transform origin-center transition-transform flex items-center justify-center cursor-pointer"
-                       style={{
-                           borderRadius: "50%",
-                           transform: `rotate(${(360 / 12) * index + 270}deg) translateX(15em)`,
-                           background: `${clickedSign === sign.name ? "radial-gradient(#17123310,#CDCBC0)" : "radial-gradient(var(--color--dark), #f5df60100)"}`,
-                       }}
-                   >
-                       <Image
-                           style={{
-                               transform: `rotate(-${(360 / 12) * index + 270}deg) `
-                           }}
-                           src={`${links}${sign.id}.png`}
-                           alt={`Zodiac Sign ${sign.name}`}
-                           width="100"
-                           height="100"
-                           className="w-8 h-auto"
-                       />
-                   </div>
-               ))}
-           </div>
-       </div>
-   );
+    const [zodiac, setZodiac] = useState<ZodiacSign[]>(Data.signs);
+    const [clickedSign, setClickedSign] = useState("unclicked");
+    const [clickedSummary, setClickedSummary] = useState("none");
+    const links = '/assets/signs/';
+
+    return (
+        <div
+            className="min-h-screen flex flex-col items-center justify-center">
+            <div
+                className="text-dark relative z-10">
+                {clickedSign === "unclicked" ?
+                    <h1 className="text-3xl mb-4 text-white">
+                        Click to see each zodiac sign
+                    </h1> :
+                    <>
+                        <div className="flex items-center jusity-center flex-col">
+                            <p className="text-3xl mb-4">{clickedSign}</p>
+                        </div>
+                        <ZodiacInfo infoSign={clickedSign} />
+                        <p>{clickedSummary}</p>
+                    </>
+                }
+            </div>
+
+            <div className="absolute flex items-center justify-center"
+                style={{
+                    height: '37em',
+                    width: '37em',
+                    background: `${clickedSign === "unclicked" ? "radial-gradient(50% 50% at 50% 50%, #3B3170 8.28%, rgba(23, 18, 51, 0.54) 75%)" : "radial-gradient(50% 50% at 50% 50%, #CDCBC0 0%, #CDCBC0 55%, rgba(205, 203, 192, 0.06) 100%)"}`,
+                    borderRadius: '100%'
+                }}>
+                {zodiac.map((sign, index) => (
+                    <div
+                        onClick={() => { setClickedSign(sign.name), setClickedSummary(sign.summary) }}
+                        key={sign.id}
+                        className="absolute w-16 h-16 transform origin-center transition-transform flex items-center justify-center cursor-pointer"
+                        style={{
+                            borderRadius: "50%",
+                            transform: `rotate(${(360 / 12) * index + 270}deg) translateX(15em)`,
+                            background: `${clickedSign === sign.name ? "radial-gradient(#17123310,#CDCBC0)" : "radial-gradient(var(--color--dark), #f5df60100)"}`,
+                        }}
+                    >
+                        <Image
+                            style={{
+                                transform: `rotate(-${(360 / 12) * index + 270}deg) `
+                            }}
+                            src={`${links}${sign.id}.png`}
+                            alt={`Zodiac Sign ${sign.name}`}
+                            width="100"
+                            height="100"
+                            className="w-8 h-auto"
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
---
+++ src/pages/components/ZodiacInfo.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+interface ZodiacInfo {
+    [key: string]: {
+        date: string;
+        personality: string;
+    };
+}
+interface ZodiacInfoProps {
+    infoSign: string;
+}
+
+const ZodiacInfo: React.FC<ZodiacInfoProps> = ({ infoSign }) => {
+    const [info, setInfo] = useState<ZodiacInfo>({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const url = 'https://zodiac-sign-api1.p.rapidapi.com/all';
+            const options = {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': '829688d398msh8a3cdce36cb0c97p1340bfjsnaf099c096a14',
+                    'X-RapidAPI-Host': 'zodiac-sign-api1.p.rapidapi.com'
+                }
+            };
+
+            try {
+                const response = await fetch(url, options);
+                const result = await response.json();
+                console.log(result);
+                setInfo(result);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="mb-2">
+        {info[infoSign] ? (
+            <>
+                <p>{info[infoSign].date}</p>
+            </>
+        ) : (
+            <p>Loading...</p>
+        )}
+    </div>
+    );
+};
+
+export default ZodiacInfo;
