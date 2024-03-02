56d4869a55b2647bc18ebd4b9122fedfa988b0c4
Sebin-Leeee <sebin.lee.work@gmail.com>
2024-03-02 00:35:28 -0800

    update compatibility

--- src/pages/components/Carousel.tsx
+++ src/pages/components/Carousel.tsx
@@ -8,13 +8,16 @@ export default function Carousel() {
   const [zodiac, setZodiac] = useState<ZodiacSign[]>(Data.signs);
   const [clickedSign, setClickedSign] = useState("unclicked");
   const [clickedSummary, setClickedSummary] = useState("none");
+  const [showCompatibilityButton, setShowCompatibilityButton] = useState(false);
   const links = "/assets/signs/";
 
   return (
-    <div className="flex flex-col items-center justify-center font-josefine"
+    <div
+      className="flex flex-col items-center justify-center font-josefine"
       style={{
-        minHeight:'80vh'
-      }}>
+        minHeight: "80vh",
+      }}
+    >
       <div className="text-dark relative z-10">
         {clickedSign === "unclicked" ? (
           <h1 className="text-3xl mb-4 text-white font-newYork">
@@ -23,9 +26,16 @@ export default function Carousel() {
         ) : (
           <>
             <div className="flex items-center jusity-center flex-col">
-              <p className="text-4xl mb-4 font-newYork font-black text-lightPurple">{clickedSign}</p>
+              <p className="text-4xl mb-4 font-newYork font-black text-lightPurple">
+                {clickedSign}
+              </p>
             </div>
             <ZodiacInfo infoSign={clickedSign} />
+            {showCompatibilityButton && (
+              <button className="bg-lightPurple text-white py-2 px-4 rounded-md mt-4">
+                Compatibility
+              </button>
+            )}
           </>
         )}
       </div>
@@ -35,28 +45,33 @@ export default function Carousel() {
         style={{
           height: "37em",
           width: "37em",
-          background: `${clickedSign === "unclicked"
-            ? "radial-gradient(50% 50% at 50% 50%, #3B3170 8.28%, rgba(23, 18, 51, 0.54) 75%)"
-            : "radial-gradient(50% 50% at 50% 50%, #CDCBC0 0%, #CDCBC0 55%, rgba(205, 203, 192, 0.06) 100%)"
-            }`,
+          background: `${
+            clickedSign === "unclicked"
+              ? "radial-gradient(50% 50% at 50% 50%, #3B3170 8.28%, rgba(23, 18, 51, 0.54) 75%)"
+              : "radial-gradient(50% 50% at 50% 50%, #CDCBC0 0%, #CDCBC0 55%, rgba(205, 203, 192, 0.06) 100%)"
+          }`,
           borderRadius: "100%",
         }}
       >
         {zodiac.map((sign, index) => (
           <div
             onClick={() => {
-              setClickedSign(sign.name), setClickedSummary(sign.summary);
+              setClickedSign(sign.name),
+                setClickedSummary(sign.summary),
+                setShowCompatibilityButton(true);
             }}
             key={sign.id}
             className="absolute w-16 h-16 transform origin-center transition-transform flex items-center justify-center cursor-pointer"
             style={{
               borderRadius: "50%",
-              transform: `rotate(${(360 / 12) * index + 270
-                }deg) translateX(15em)`,
-              background: `${clickedSign === sign.name
-                ? "radial-gradient(#17123310,#CDCBC0)"
-                : "radial-gradient(var(--color--dark), #f5df60100)"
-                }`,
+              transform: `rotate(${
+                (360 / 12) * index + 270
+              }deg) translateX(15em)`,
+              background: `${
+                clickedSign === sign.name
+                  ? "radial-gradient(#17123310,#CDCBC0)"
+                  : "radial-gradient(var(--color--dark), #f5df60100)"
+              }`,
             }}
           >
             <Image
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import Carousel from "./components/Carousel";
 import Navbar from "./components/navbar/navbar";
-import Footer from '@/pages/components/footer'
+import Footer from "@/pages/components/footer";
 
 const Home = () => {
   return (
-    <div className="bg-dark h-screen font-josefine">
-      <Navbar />
-      <Carousel />
-      <Footer />
-    </div>
+    <>
+      <div className="bg-dark h-screen font-josefine">
+        <Navbar />
+        <Carousel />
+        <Footer />
+      </div>
+    </>
   );
 };
 
