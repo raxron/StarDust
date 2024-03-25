c75409979fb9984564aa64733419903bb75af735
Sierra Laurent <sierraclauren@gmail.com>
2024-03-25 10:25:22 -0700

    added favicon plus

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Head from "next/head";
 import Carousel from "./components/Carousel";
 import ZodiacForm from "./components/ZodiacForm";
 import Navbar from "./components/navbar/navbar";
@@ -7,6 +8,11 @@ import Footer from '@/pages/components/footer'
 const Form = () => {
   return (
     <>
+    <Head>
+        <title>STARDUST | Form</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
       <div className="bg-dark font-josefine">
         <Navbar />
         <ZodiacForm />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Head from "next/head";
 import Carousel from "./components/Carousel";
 import Navbar from "./components/navbar/navbar";
 import Footer from "@/pages/components/footer";
@@ -6,6 +7,11 @@ import Footer from "@/pages/components/footer";
 const Home = () => {
   return (
     <>
+    <Head>
+        <title>STARDUST | Welcome</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
       <div className="bg-dark h-screen font-josefine">
         <Navbar />
         <Carousel />
